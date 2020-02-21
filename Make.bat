2e25f17fccb10f65499e3ee99810440488aba4bb
TheDeadFish <michaelstamper1@gmail.com>
2020-02-21 15:01:58 +0000

    1. replaced make.bat with cmake

--- Make.bat
+++ Make.bat
@@ -1,6 +1,9 @@
-setlocal
-call egcc.bat
-windres resource.rc -O coff -o resource.o
-set LFLAGS=%LFLAGS% -Wl,--large-address-aware -mwindows
-set LIBS=-lwin32hlp -lexshit -lstdshit -lcomdlg32 -luuid
-gcc main.cc findcore.cc resource.o %CCFLAGS2% %LFLAGS% %LIBS% -o findText.exe
+@setlocal
+@call egcc.bat
+@if not exist build32 mkdir build32
+@set CFLAGS=%CFLAGS2%
+@set CXXFLAGS=%CCFLAGS2%
+@pushd build32
+@%CMAKE%  ..\. -DCMAKE_BUILD_TYPE=Release
+@mingw32-make
+@popd
